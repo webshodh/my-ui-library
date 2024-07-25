6992dd8bbc49da0cb7fa1257b1c624cbaa72ea3a
VISHAL VIJAY GHOLKAR <49200013+Vishal7674@users.noreply.github.com>
2024-07-25 22:13:38 +0530

    Updated

--- my-ui-library/src/index.js
+++ my-ui-library/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// In your UI library's index.js or similar file
+
+export { default as AlertMessage } from '../src/Components/AlertMessage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
